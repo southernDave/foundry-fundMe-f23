3f60007a5ac9f0c1c80bf0933e04e964282ecf72
david <obohdavid2014@example.com>
2023-06-15 20:20:26 +0100

    my umpteenth commit

---
+++ script/Interactions.s.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT;
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevopsTools.sol";
+
+contract FundFundMe is Script{
+
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        fundFundMe(mostRecentlyDeployed);
+    }
+}
+
+contract withdrawFundMe is Script {
+
+}
